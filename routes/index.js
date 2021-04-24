8ae53140e23adb502cb615ff3dd1a35c0ac2fa08
Spence Patrick <spence_patrick@intuit.com>
2021-04-24 15:29:36 -0600

    move require

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 
 var express = require('express');
 var router = express.Router();
+const puppeteer = require('puppeteer');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +11,13 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/custom-meal-planner', function(req, res, next) {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  await page.goto('http://www.example.com/');
+  console.log('page?', page);
   res.render('meal-plan', { title: 'Meal-Plan Page'});
 });
 
