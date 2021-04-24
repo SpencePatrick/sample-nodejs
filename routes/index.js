9383c0ec30550396698d1bdec264b894c1c3dfad
Spence Patrick <spence_patrick@intuit.com>
2021-04-24 16:03:29 -0600

    we will get this to work

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@
 var express = require('express');
 var router = express.Router();
 const fetch = require("node-fetch");
-
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,16 +13,23 @@ router.get('/', function(req, res, next) {
 
 router.get('/custom-meal-planner', function(req, res, next) {
   (async () => {
-    console.log("starting to fetch");
-    const response = await fetch('https://www.allrecipes.com/recipe/22831/alfredo-sauce/');
-    console.lot("response", response);
-    const text = await response.text();
-    console.log("text", text);
-    const dom = await new JSDOM(text);
-    const ingredientsSection = dom.window.document.querySelector(".ingredients-section").textContent
-    console.log(dom.window.document.querySelector(".ingredients-section").textContent);
-    console.log('document', document);
-    document.querySelector("#ingredient-section").appendChild(ingredientsSection);
+    try {
+      const fetch = require("node-fetch");
+
+      console.log("starting to fetch");
+      const response = await fetch('https://www.allrecipes.com/recipe/22831/alfredo-sauce/');
+      console.lot("response", response);
+      const text = await response.text();
+      console.log("text", text);
+      const dom = await new JSDOM(text);
+      const ingredientsSection = dom.window.document.querySelector(".ingredients-section").textContent
+      console.log(dom.window.document.querySelector(".ingredients-section").textContent);
+      console.log('document', document);
+      document.querySelector("#ingredient-section").appendChild(ingredientsSection);
+    } catch(err) {
+      console.log(err);
+    }
+    
   });
   res.render('meal-plan', { 
     title: 'Meal-Plan Page'
