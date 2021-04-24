cb6c764fd563cfbd87f8ce0aaf62978520bd6389
Spence Patrick <spence_patrick@intuit.com>
2021-04-24 16:42:38 -0600

    trigger something

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,14 @@ router.get('/custom-meal-planner', function(req, res, next) {
   const browser = await puppeteer.launch({
     headless: false,
   });
+  console.log("browser", browser);
   const page = await browser.newPage();
+  console.log("page1", page);
   await page.setRequestInterception(true);
   await page.goto('https://www.allrecipes.com/recipe/22831/alfredo-sauce/');
   console.log("page", page);  
+  console.log(page.window.document.querySelector(".ingredients-section").textContent);
+
   res.render('meal-plan', { 
     title: 'Meal-Plan Page'
   });
