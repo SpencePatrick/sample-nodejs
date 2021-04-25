b3f6b08f18a940e190d95d7f4ad744b763f803b9
Spence Patrick <spence_patrick@intuit.com>
2021-04-24 20:34:09 -0600

    adding somethiung

--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,21 @@ router.get('/custom-meal-planner', function(req, res, next) {
   console.log('going in');
   (async () => {
     const puppeteer = require('puppeteer');
-
-    const browser = await puppeteer.launch({
-      headless: false,
-    });
-    console.log("browser", browser);
-    const page = await browser.newPage();
-    console.log("page1", page);
-    await page.setRequestInterception(true);
-    await page.goto('https://www.allrecipes.com/recipe/22831/alfredo-sauce/');
-    console.log("page", page);  
-    console.log(page.window.document.querySelector(".ingredients-section").textContent);
+    try {
+      const browser = await puppeteer.launch({
+        headless: false,
+      });
+      console.log("browser", browser);
+      const page = await browser.newPage();
+      console.log("page1", page);
+      await page.setRequestInterception(true);
+      await page.goto('https://www.allrecipes.com/recipe/22831/alfredo-sauce/');
+      console.log("page", page);  
+      console.log(page.window.document.querySelector(".ingredients-section").textContent);
+    } catch (err) {
+      console.log(err);
+    }
+    
   })();
   res.render('meal-plan', { 
     title: 'Meal-Plan Page'
