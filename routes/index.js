16ad79899e743993482eee00acf2143a28323809
Spence Patrick <spence_patrick@intuit.com>
2021-04-24 21:04:26 -0600

    phantom

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,22 @@ router.get('/custom-meal-planner', function(req, res, next) {
   (async () => {
     try {
       console.log('in line flin');
+      phantom.create(function (ph) {
+        ph.createPage(function (page) {
+          var url = "http://www.bdtong.co.kr/index.php?c_category=C02";
+          page.open(url, function() {
+            page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+              page.evaluate(function() {
+                $('.listMain > li').each(function () {
+                  console.log($(this).find('a').attr('href'));
+                });
+              }, function(){
+                ph.exit()
+              });
+            });
+          });
+        });
+      });
     } catch (err) {
       console.log(err);
     }
